815b4a17cfbecdbc85a86663880da366440c6388
Adhip Jajoo <109216150+Aquarius563@users.noreply.github.com>
2024-07-03 22:31:29 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,7 +1,7 @@
 # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Aquarius563
    # Input:
    # p, principal amount
    # t, time period in years
